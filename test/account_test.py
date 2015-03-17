c098f6983c7ef8caeb97317ff078b83022304a5b
Radovan Lekanovic <lekanovic@gmail.com>
2015-03-17 16:56:52 -0700

    Add testcase for account.

---
+++ test/account_test.py
@@ -0,0 +1,31 @@
+import unittest
+import sys
+sys.path.append('..')
+from account import Account
+
+class AccountTestCase(unittest.TestCase):
+
+	def test_from_json(self):
+		doc = {}
+		doc['status'] = "active"
+		doc['public_key'] = "tpubDCVcrTzunZwuc67hSQHmjHN8efpCVw4aZDUqvztyryj8QDpsvjxipein85QKt3ZuWXGapnuYVBEUGyvAQMJBNpruqxqStQ5RdrLhRCzNtuc"
+		doc['date'] = "2015-03-17 14:04:17.578191"
+		doc['name'] = "Maja"
+		doc['account_index'] = "1"
+		doc['passwd'] = "password2"
+		doc['lastname'] = "Lekanovic"
+		doc['wallet-balance'] = 0
+		doc['email'] = "majasusa@hotmail.com"
+
+		account = Account(doc, network="testnet")
+
+		self.assertEqual(account.name, "Maja")
+		self.assertEqual(account.lastname, "Lekanovic")
+		self.assertEqual(account.passwd, "password2")
+		self.assertEqual(account.email, "majasusa@hotmail.com")
+		self.assertEqual(account.account_index, "1")
+		self.assertEqual(account.status, "active")
+		self.assertEqual(account.public_key, "tpubDCVcrTzunZwuc67hSQHmjHN8efpCVw4aZDUqvztyryj8QDpsvjxipein85QKt3ZuWXGapnuYVBEUGyvAQMJBNpruqxqStQ5RdrLhRCzNtuc")
+
+if __name__ == '__main__':
+	unittest.main()
