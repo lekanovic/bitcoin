dcd333da4bfb12285a7877ee0caeacd53fc93441
Radovan Lekanovic <lekanovic@gmail.com>
2015-04-04 15:00:21 -0700

    Bugfix in the testcase wrong path.

--- picunia/test/balance_test.py
+++ picunia/test/balance_test.py
@@ -2,12 +2,17 @@ import unittest
 import sys
 import json
 sys.path.append('..')
-from picunia.database.storage import Storage
+from database.storage import Storage
 from users.account import Account
 
 class BalanceTestCase(unittest.TestCase):
 
 	def test_check_balance(self):
+		'''
+		This test will check all accounts from the database
+		and their wallet_balance. And compare them to the
+		actual balance by checking spendables from the blockchain.
+		'''
 		db = Storage()
 
 		accounts = [json.loads(a) for a in db.get_all_accounts()]
