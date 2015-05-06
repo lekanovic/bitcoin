932b3e9a815eb4ef24e52bf7b764a46e8af34dae
Radovan Lekanovic <lekanovic@gmail.com>
2015-05-05 20:13:46 -0700

    Adding new config module where all the config values are stored.

---
+++ picunia/config/settings.py
@@ -0,0 +1,8 @@
+# Global settings file
+
+class Settings():
+	MONGODB_ADDRESS = 'localhost'
+	MONGODB_PORT = '27017'
+	INSIGHT_ADDRESS = 'http://localhost:3001'
+	GAP_LIMIT = 5
+	NETWORK = 'testnet'
