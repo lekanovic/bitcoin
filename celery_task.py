6c2a96cf9d81b0f28375293f3e37e60ada71898b
Radovan Lekanovic <lekanovic@gmail.com>
2015-06-12 19:37:22 -0700

    Implement rpc calls for all the different payment methods.
    This will be the methods that are exposed to the web service using
    celery tasks.

--- celery_task.py
+++ celery_task.py
@@ -18,13 +18,10 @@ def create_account_rpc(name,lastname,email,password):
 	start_service()
 	time.sleep(2)
 
-	create_account(name,lastname,email,password.encode('utf-8'))
+	key_handler = create_account(name,lastname,email,password.encode('utf-8'))
 
-	while True:
-		wallet = fetch_wallet(email)
-		if wallet['public_key']:
-			break
-		time.sleep(2)
+	while not key_handler.has_been_called:
+		time.sleep(0.5)
 
 	return "Account %s created" % (email)
 
@@ -33,3 +30,41 @@ def fetch_account_rpc(email):
 	a = fetch_account(email)
 	return a
 
+@app.task
+def pay_to_address_rpc(send_from, send_to, amount, msg):
+	print send_from, send_to, amount, msg
+	start_service()
+	time.sleep(2)
+
+	transaction_handler = pay_to_address(send_from, send_to, amount, msg=msg)
+
+	while not transaction_handler.has_been_called:
+		time.sleep(0.5)
+
+	return "STANDARD transaction %s created" % (transaction_handler.tx_info['tx_id'])
+
+@app.task
+def multisig_transacion_rpc(from_email, to_email, escrow_email, amount, msg):
+	print from_email, to_email, escrow_email, amount, msg
+	start_service()
+	time.sleep(2)
+
+	transaction_handler = multisig_transacion(from_email, to_email, escrow_email, amount, msg=msg)
+
+	while not transaction_handler.has_been_called:
+		time.sleep(0.5)
+
+	return "MULTISIG transaction %s created" % (transaction_handler.tx_info['tx_id'])
+
+@app.tasks
+def write_blockchain_message_rpc(email, message):
+	print email, message
+	start_service()
+	time.sleep(2)
+
+	transaction_handler = write_blockchain_message(email, message)
+
+	while not transaction_handler.has_been_called:
+		time.sleep(0.5)
+
+	return "BLKCHN_MESSAGE transaction %s created" % (transaction_handler.tx_info['tx_id'])
