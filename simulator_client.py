b54de7621fa7e3573a7a48052f150b0dee6bb736
Radovan Lekanovic <radovan.lekanovic@room5.com>
2015-07-03 14:43:32 +0200

    Adding a realistic simulator of picunia.

--- simulator_client.py
+++ simulator_client.py
@@ -3,8 +3,11 @@ from celery_task import fetch_account_rpc
 from celery_task import pay_to_address_rpc
 from celery_task import validate_passwd_rpc
 from celery_task import find_account_with_balance_rpc
+from celery_task import fetch_transaction_by_email_rpc
+from celery_task import multisig_transacion_rpc
+from celery_task import write_blockchain_message_rpc
 import time, sys, argparse
-import urllib, json
+import urllib, json, datetime
 from random import randint
 
 '''
@@ -43,6 +46,7 @@ while not valid.ready():
 print valid.get()
 '''
 joke_database = []
+async_results = []
 
 def fetch_jokes():
     url = "http://api.icndb.com/jokes/random/1000"
@@ -57,27 +61,29 @@ def get_chucknorris_joke():
 	return joke_database.pop()
 
 def send_chucknorris_joke_as_proofofexistens(from_email):
-	db = Storage()
 	tx_unsigned = 0
 	proofofexistens_msg = get_chucknorris_joke()
 
-	write_blockchain_message(from_email, proofofexistens_msg)
+	write_blockchain_message_rpc.delay(from_email, proofofexistens_msg)
 
 def call_api(items=0):
 	url = "http://api.randomuser.me/?results=%d" % items
 	response = urllib.urlopen(url);
 	data = json.loads(response.read())
 
+	res_array = []
 	for d in data['results']:
 		name = d["user"]["name"]["first"]
 		lastname = d["user"]["name"]["last"]
 		passwd = d["user"]["password"]
 		email = d["user"]["email"]
-		create_account_rpc.delay(name, lastname, email, passwd.encode('utf-8'))
+		res = create_account_rpc.delay(name, lastname, email, passwd.encode('utf-8'))
+		res_array.append(res)
+	return res_array
 
 def fill_database(items=1):
 	if items <= 100:
-		call_api(items)
+		return call_api(items)
 	else:
 		n = items / 100
 		for p in range(n):
@@ -93,7 +99,7 @@ def find_account_with_balance():
 
 	value =  account.get()
 	value = json.loads(value)
-	print value, value['balance']
+
 	return value, value['balance']
 
 def one_round():
@@ -101,21 +107,62 @@ def one_round():
 	receiver, tmp = find_account_with_balance()
 	amount = balance / 10
 
+	print "%s sending %d to %s amount %d" % (sender['email'], amount, receiver['email'], amount)
+	pay_to_address_rpc.delay(sender['email'], receiver['email'], amount, msg="This is a test message")
+
+def simulate():
+	global async_results
+	print "New round: %s" % datetime.datetime.now()
+
+	if randint(0,50) == 10:
+		print "Generating new account"
+		res_array = generate_accounts(1)
+		async_results.extend(res_array)
+
 	if randint(0,20) == 10:
-		send_chucknorris_joke_as_proofofexistens(sender['email'])
+		sender, _ = find_account_with_balance()
+		print "%s creating proof-of-existence message" % sender['email']
+		res = write_blockchain_message_rpc.delay(sender['email'], get_chucknorris_joke())
+		async_results.append(res)
 
-	#if randint(0,20) == 10:
-	#    escrow, tmp = find_account_with_balance()
-	#    multisig_2of3(sender['email'], receiver['email'], escrow['email'], amount)
+	if randint(0,20) == 10:
+		sender, balance = find_account_with_balance()
+		receiver, _ = find_account_with_balance()
+		escrow, _ = find_account_with_balance()
+		amount = balance / 10
 
-	print "%s sending %d to %s" % (sender['email'], amount, receiver['email'])
-	send_from_to(sender['email'], receiver['email'], amount)
+		print "Multisig %s -> %s |escrow %s| SAT: %d" % (sender['email'], receiver['email'], escrow['email'], amount)
+		res = multisig_transacion_rpc.delay(sender['email'], receiver['email'], escrow['email'], amount, msg="Multisig test")
+		async_results.append(res)
 
-def send_from_to(from_email, to_email, amount):
-	tx = pay_to_address_rpc.delay(from_email, to_email, amount, msg="This is a test message")
+	if randint(0,10) == 5:
+		sender, balance = find_account_with_balance()
+		receiver, tmp = find_account_with_balance()
+		amount = balance / 10
+
+		print "%s -> %s SAT: %d" % (sender['email'], receiver['email'], amount)
+		res = pay_to_address_rpc.delay(sender['email'], receiver['email'], amount, msg="This is a test message")
+		async_results.append(res)
+
+	if randint(0,20) == 10:
+		sender, _ = find_account_with_balance()
+
+		print "Transaction INFO for user %s" % sender['email']
+		res = fetch_transaction_by_email_rpc.delay(sender['email'])
+		async_results.append(res)
+
+	for r in async_results[:]:
+		if r.ready():
+			print "ASYNC message received:"
+			print r.get()
+			async_results.remove(r)
+
+	print "Messages in pipe %d" % (len(async_results))
+	sleep_time = randint(3, 30)
+	time.sleep(sleep_time)
 
 def generate_accounts(n):
-	fill_database(n)
+	return fill_database(n)
 
 def main(argv):
 	msg = "Test that simulates new accounts created. Simulates user sending Satoshis between them."
@@ -124,7 +171,8 @@ def main(argv):
 		help="Generate random user accounts. Input: int - number of accounts to create")
 	parser.add_argument("-s","--simulate",
 		help="Simulate random user sending Satoshi to another random user. Input: int - rounds to run simulator")
-
+	parser.add_argument("-f","--forever", action='store_true',
+		help="This will simulate usage of picunia and run forever")
 	args = parser.parse_args()
 
 	if args.generate:
@@ -135,7 +183,12 @@ def main(argv):
 		number = int(args.simulate)
 		for i in range(0, number):
 			one_round()
-			time.sleep(5)
+			sleep_time = randint(10, 300)
+			time.sleep(sleep_time)
+
+	if args.forever:
+		while True:
+			simulate()
 
 	time.sleep(1000000)
 
