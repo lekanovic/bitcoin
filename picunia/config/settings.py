f884ef75a2202127a4d82fe1786f69b0370bcc02
Radovan Lekanovic <radovan.lekanovic@room5.com>
2015-07-06 23:45:32 +0200

    Addin setting for dynamic class loader.
    We want this so that we can switch if we want to use RPI HW as a sign server or if
    we want to do the signing on the local computer. This is not secure but its great
    when developing.

--- picunia/config/settings.py
+++ picunia/config/settings.py
@@ -6,15 +6,23 @@ class Settings():
 	INSIGHT_ADDRESS = 'http://localhost:3001'
 	GAP_LIMIT = 5
 	NETWORK = 'testnet'
+	USE_RPI_EMULATOR = True
+	RSCODEC_NSYM = 64
+	USE_COMPRESSION = True
+	BAUD_MINIMODEM = '3000'
 	if NETWORK == 'testnet':
 		NETCODE = 'XTN'
 		KEY_PATHS = "44H/1H/"
 	else:
 		NETCODE = 'BTC'
 		KEY_PATHS = "44H/0H/"
-	RSCODEC_NSYM = 64
-	USE_COMPRESSION = True
-	BAUD_MINIMODEM = '3000'
-	WIFS_DB = '/raid10/wifs.db'
-	KEYS_DB = '/raid10/key.db'
-	SEED_FILE = '/raid10/masterseed'
+	if USE_RPI_EMULATOR:
+		SIGN_TX_PATH = "picunia.security.sign_tx_client_simulator"
+		WIFS_DB = 'wifs.db'
+		KEYS_DB = 'key.db'
+		SEED_FILE = 'masterseed'
+	else:
+		SIGN_TX_PATH = "picunia.security.sign_tx_client"
+		WIFS_DB = '/raid10/wifs.db'
+		KEYS_DB = '/raid10/key.db'
+		SEED_FILE = '/raid10/masterseed'
