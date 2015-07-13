c3747831307e9949da0941b40c5c27e31090b2ff
Radovan Lekanovic <radovan.lekanovic@room5.com>
2015-07-13 18:48:15 +0200

    Code refactoring.
     * Only have one single callback function
     * Update test functions

--- picunia/security/sign_tx_client.py
+++ picunia/security/sign_tx_client.py
@@ -56,7 +56,7 @@ class Receiver:
             self.stdout = stdout
             self.stderr = stderr
             self.compress = compress
-            self.func_cb = []
+            self.func_cb = None
             self.event = event
             self.signal = True
 
@@ -148,9 +148,7 @@ class Receiver:
                             continue
 
                         # Callback function with the signed transaction
-                        cb = self.func_cb.pop(0)
-                        if cb is not None:
-                            cb(p.tx)
+                        self.func_cb(tx)
 
                         resend_package = False
                         self.event.set()
@@ -183,7 +181,7 @@ def start_service():
     consumer.setDaemon(True)
     consumer.start()
 
-    #receiver.p.wait()
+    time.sleep(1)
 
 def stop_service():
     global receiver
@@ -198,9 +196,9 @@ def sign_tx(wallet_index, key_index, tx, cb):
     if not isinstance(key_index, list):
         raise TypeError("Expected list, got %s" % (type(key_index),))
     if not isinstance(tx, unicode):
-        raise TypeError("Expected int, got %s" % (type(tx),))
+        raise TypeError("Expected unicode, got %s" % (type(tx),))
 
-    receiver.reader.func_cb.append(cb)
+    receiver.reader.func_cb = cb
 
     package = assemble_package(wallet_index, key_index, tx, rtype="TXN")
 
@@ -209,7 +207,7 @@ def sign_tx(wallet_index, key_index, tx, cb):
 def request_public_key(wallet_index, cb):
     global receiver
 
-    receiver.reader.func_cb.append(cb)
+    receiver.reader.func_cb = cb
 
     # Since we are requesting a public key we can leave tx empty.
     # And key_index is zero since this is the first key.
@@ -219,29 +217,27 @@ def request_public_key(wallet_index, cb):
 
 
 # TESTING ----------------------------
-'''
-import time
-start_service()
-time.sleep(5)
-
 def callback(tx_hex):
-    print "callback called"
+    print "+++++++++callback called+++++++++"
     print tx_hex
 
-for i in range(0,5):
-    request_public_key(i, callback)
+def test_key():
+    start_service()
+    time.sleep(2)
 
-time.sleep(500000)
+    request_public_key(30, callback)
 
+    time.sleep(30)
 
+def test_sign():
+    start_service()
+    time.sleep(2)
 
-t = u'01000000013ee4638534d6a48979e79fad3a98158f0c1265f247560af07ec20fb1488d0b910000000000ffffffff02ecff0000000000001976a914fd906708ad09ee08bc03cda5db4983e3a817b73b88ac593b0800000000001976a9145e518af5696b29693c266086d46fd73943d87d1f88ac0000000055620900000000001976a9143575a1070562bc3d348b557a9e51722e2878697288ac' 
-sign_tx(4657,4, t, callback)
-time.sleep(500000)
+    t = u"0100000001a3a2702d3c1ffe732ec5104ee215e06e2162815069c39a36943dc2c2e1dabd540000000000ffffffff0300000000000000002a6a28436875636b204e6f727269732066696e697368656420576f726c64206f662057617263726166742e10270000000000001976a9149f35ecd88caf647acec5445728bd667a8adb7f3488acf6726600000000001976a914d50a29953fe8fa5c8d18ed60c6400fc7c508b79a88ac0000000016c16600000000001976a914d17fd859759cce0aec42857a06e9adc70354cc5d88ac"
+ 
+    sign_tx(3,[117], t, callback)
+    time.sleep(30)
 
+#test_sign()
+#test_key()
 
-sign_tx(4967,8,'XTN', '0100000002d636cae1cb2db9f586ae4e7cccd56e613f87cc6bfa687f659de2b26e65b5fb1b0100000000ffffffff1a054bb7b551b296d7bfdddda81c591430da90f5b9ac28fe07391b5646c8da710000000000ffffffff02a0050000000000001976a9146be7733f895908c7a8e412e29580c88bdb105c7488ac930b0000000000001976a914a6a2371e74f88918dd705d9a23a7d1ac991046f588ac000000001d2b0000000000001976a914a6a2371e74f88918dd705d9a23a7d1ac991046f588ac260d0000000000001976a914a41d436944bec4107f7124463071c9adbcf5b6ca88ac')
-sign_tx(925,10,'XTN', '0100000001f87239ac1658887a50e5f0a9b85b5932625cf5327429acaf5a7d922b8cd81f240100000000ffffffff0225220000000000001976a914841de1c8c900a6902528f8924254f8da56c559e388ac27c00000000000001976a9149476f505c1b7d7b31e1ad1fae8acf967a38210ba88ac000000005c090100000000001976a9149476f505c1b7d7b31e1ad1fae8acf967a38210ba88ac')
-sign_tx(6865,2,'XTN', '010000000152ee011e4d01b7db9a7940f9440cd28688722e3905457f64da5f4fc5e8c7c16e0100000000ffffffff0292250000000000001976a91459d55e2898fc2016c1d4a11d61dadff348b3ecd888ac142b0100000000001976a914328047f57b5bb98b668a6e0fa5180babf7e027b988ac00000000b6770100000000001976a914328047f57b5bb98b668a6e0fa5180babf7e027b988ac')
-sign_tx(692,4,'XTN' , '0100000001efb7a4c1cecf9fbed9e3c368b3ef00116788285b036596a9fc4162124e559bb10100000000ffffffff0271110000000000001976a9144bb36dce89a414b18dd3e7adf2ff996f46ead9a488acac6a0000000000001976a914e29d09e7e5c5c04bb28e3eaa002d26af3c3ba99e88ac000000002da30000000000001976a914e29d09e7e5c5c04bb28e3eaa002d26af3c3ba99e88ac')
-'''
